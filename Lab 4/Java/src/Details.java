840b88138c0755353708a08b6b031864b8a4d738
Zethyst <zethyst@protonmail.com>
2023-02-16 13:15:50 +0530

    Added Lab 4

--- Lab 4/Java/src/Details.java
+++ Lab 4/Java/src/Details.java
@@ -3,26 +3,27 @@ import java.util.*;
 class Details {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String name[]=new String[3];
-        int roll[]=new int[3];
-        String sec[]=new String[3];
-        String Br[]=new String[3];
+        String name[] = new String[3];
+        int roll[] = new int[3];
+        String sec[] = new String[3];
+        String Br[] = new String[3];
         for (int i = 0; i < 3; i++) {
-            System.out.println("\nUser "+(i+1)+":");
+            System.out.println("\nUser " + (i + 1) + ":");
             System.out.println("Enter your details below: ");
             System.out.print("Name: ");
-            name[i]=sc.next();
+            sc.skip("\\R?");
+            name[i] = sc.nextLine();
             // sc.nextLine();
             System.out.print("Roll No.: ");
-            roll[i]=sc.nextInt();
+            roll[i] = sc.nextInt();
             System.out.print("Section: ");
-            sec[i]=sc.next();
+            sec[i] = sc.next();
             System.out.print("Branch: ");
-            Br[i]=sc.next();
+            Br[i] = sc.next();
         }
-        System.out.println("\nName \t Roll No \t Section \t Branch");
+        System.out.println("\n Name \t Roll No \t Section \t Branch");
         for (int i = 0; i < 3; i++) {
-            System.out.println(name[i]+"\t"+roll[i]+"\t"+sec[i]+"\t"+Br[i]);
+            System.out.println(name[i] + "\t" + roll[i] + "\t" + sec[i] + "\t" + Br[i]);
         }
     }
 }
